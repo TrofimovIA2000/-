162578932cc9d9d11617ebd00d12dfccb758a547
TrofimovIA2000 <76566187+TrofimovIA2000@users.noreply.github.com>
2020-12-25 01:58:02 +0300

    Update ClientHandler.java

--- Practice#4/client/ClientHandler.java
+++ Practice#4/client/ClientHandler.java
@@ -19,7 +19,7 @@ public class ClientHandler extends SimpleChannelInboundHandler<String>{
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        // Close the connection when an exception is raised.
+        
         cause.printStackTrace();
         ctx.close();
     }
